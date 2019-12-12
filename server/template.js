bd4e19b0d4f6f1282f8aba5e03dab754bf0b17b7
maxlath <k@maxlath.eu>
2019-12-12 11:56:49 +0100

    template: add links sections

--- server/template.js
+++ server/template.js
@@ -22,13 +22,18 @@ module.exports = hashtag => `<!DOCTYPE html>
 </head>
 <body>
   <h1>#${hashtag}</h1>
-  <ul>${getLinksList(hashtag)}</ul>
+  <h2>Social Media</h2>
+  <ul>${getLinksList(socialMedia, hashtag)}</ul>
+  <h2>Images</h2>
+  <ul>${getLinksList(imagePlatforms, hashtag)}</ul>
+  <h2>Back to Wikimedia sites</h2>
+  <ul>${getLinksList(wikimediaSites, hashtag)}</ul>
 </body>
 </html>
 `
 
-const getLinksList = hashtag => {
-  return platforms
+const getLinksList = (list, hashtag) => {
+  return list
   .map(getlink(hashtag))
   .join('\n')
 }
@@ -42,10 +47,18 @@ const getlink = hashtag => platform => {
   </li>`
 }
 
-const platforms = [
+const socialMedia = [
   { name: 'Mastodon', formatter: 'https://mastodon.social/tags/$1' },
   { name: 'Twitter', formatter: 'https://twitter.com/hashtag/$1' },
   { name: 'Facebook', formatter: 'https://facebook.com/hashtag/$1' },
+]
+
+const imagePlatforms = [
   { name: 'Flickr', formatter: 'https://www.flickr.com/photos/tags/$1' },
   { name: 'Gfycat', formatter: 'https://gfycat.com/fr/gifs/tag/$1' }
+]
+
+const wikimediaSites = [
+  { name: 'Wikidata', formatter: 'https://tools.wmflabs.org/hub/P2572:$1?site=wikidata' },
+  { name: 'Wikipedia', formatter: 'https://tools.wmflabs.org/hub/P2572:$1?site=wikipedia' }
 ]
